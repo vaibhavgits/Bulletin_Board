a603e6f9866c580068a74266e6de15f3c5531961
yashjain9953 <yashjain9953@gmail.com>
2021-06-09 20:32:29 +0530

    added branches

--- loginpage/branchboard/urls.py
+++ loginpage/branchboard/urls.py
@@ -5,5 +5,7 @@ app_name = 'branchboard'
 urlpatterns = [
 	path('student', views.student),
 	path('teacher', views.teacher),
-	path('dashboard/', include('dashboard.urls')),
+	# path('dashboard/', include('dashboard.urls')),
+	path('student/<branch>/dashboard/', include('dashboard.urls')),
+	path('teacher/<branch>/dashboard/', include('dashboard.urls')),
 ]
--- loginpage/dashboard/views.py
+++ loginpage/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def student(request):
+def student(request, branch):
 	return render(request, 'dashboard/dashboard.html', {'student':True, 'teacher':False})
-def teacher(request):
+def teacher(request, branch):
 	return render(request, 'dashboard/dashboard.html', {'student':False, 'teacher':True})

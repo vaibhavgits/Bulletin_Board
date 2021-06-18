d69874c1a4a30a174a3d93905a7b6eff517dee5c
Yash Jain <yashjain9953@gmail.com>
2021-06-19 00:00:58 +0530

    Added model for file upload

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

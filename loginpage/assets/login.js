e5844eb077907a75d18c2a182a279c13b7c0ec91
Yash Jain <yashjain9953@gmail.com>
2021-07-01 09:16:19 +0530

    enter key will sign in now

--- loginpage/assets/login.js
+++ loginpage/assets/login.js
@@ -1,22 +1,26 @@
-// For teachers
+
 document.getElementById("student").addEventListener("click", signinFunction);
 document.getElementById("teacher").addEventListener("click", signinFunction);
+document.getElementById("pass-student").addEventListener("keyup", signinFunction)
 // var csspage="";
-function signinFunction() {
-    const usernameStudent = document.getElementById("user-student").value;
-    const passwordStudent = document.getElementById("pass-student").value;
-    const usernameTeacher = document.getElementById("user-teacher").value;
-    const passwordTeacher = document.getElementById("pass-teacher").value;
-    if (usernameStudent === "admin-student" && passwordStudent === "admin-student") { 
-        window.location.href="branchboard/student";
-        // csspage="student";
+function signinFunction(event) {
+    // window.alert(event.type);
+    if(event.type === "click" || (event.type === "keyup" && event.keyCode === 13)){
+        const usernameStudent = document.getElementById("user-student").value;
+        const passwordStudent = document.getElementById("pass-student").value;
+        const usernameTeacher = document.getElementById("user-teacher").value;
+        const passwordTeacher = document.getElementById("pass-teacher").value;
+        if (usernameStudent === "admin-student" && passwordStudent === "admin-student") { 
+            window.location.href="branchboard/student";
+            // csspage="student";
+        }
+        else if (usernameTeacher === "admin-teacher" && passwordTeacher === "admin-teacher") {
+            window.location.href="branchboard/teacher";
+            // csspage="teacher";
+        }
+        else {
+            alert("Incorrect Credentials!" );
+        }
     }
-    else if (usernameTeacher === "admin-teacher" && passwordTeacher === "admin-teacher") {
-        window.location.href="branchboard/teacher";
-        // csspage="teacher";
-    }
-    else {
-        alert("Incorrect Credtentials!" );
-    } 
+    
 }
-
